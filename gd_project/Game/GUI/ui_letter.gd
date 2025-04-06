4b6a3450e1bbbcac74294ad5e411284bb554456b
Félix <someonefefe@gmail.com>
2025-04-07 00:26:16 +0200

    work on letter room

--- gd_project/Game/GUI/ui_letter.gd
+++ gd_project/Game/GUI/ui_letter.gd
@@ -3,11 +3,19 @@ extends Control
 signal on_letter_selected
 
 func initialize(letter : String):
-	$Button.text = letter
+	%Letter.text = letter
 
 
 func get_letter() -> String:
-	return $Button.text
+	return %Letter.text
 	
 func _on_button_pressed():
 	on_letter_selected.emit()
+
+
+func _on_button_mouse_entered() -> void:
+	position.y -= 5
+
+
+func _on_button_mouse_exited() -> void:
+	position.y += 5 # Replace with function body.
