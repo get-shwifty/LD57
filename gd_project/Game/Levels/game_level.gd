6d08a15a1fb6b1c7fba4881f5e6cfa4d06329cc9
tyra <someonefefe@gmail.com>
2025-04-07 02:31:59 +0200

    fix level

--- gd_project/Game/Levels/game_level.gd
+++ gd_project/Game/Levels/game_level.gd
@@ -75,7 +75,7 @@ func start_word_compose():
 	ui_container.add_child(word_composing_menu)
 	word_composing_menu.on_word_confirmed.connect(confirm_word)
 	word_composing_menu.on_menu_closed.connect(close_word_compose)
-	word_composing_menu.initialize(self)
+	#word_composing_menu.initialize(self)
 
 func close_word_compose():
 	word_composing_menu.queue_free()
