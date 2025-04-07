36767ce4c5a69454b5280845c5e46ee948de6a2f
Alexis Schad <a@mandelbro.com>
2025-04-08 00:18:26 +0200

    room w/h for camera

--- gd_project/Game/Levels/room.gd
+++ gd_project/Game/Levels/room.gd
@@ -1,7 +1,15 @@
 extends Node
 class_name LevelRoom
 
+@export var width: int = 320 * 2
+@export var height: int = 180 * 2
+
 signal on_captured(letter)
+
+func _ready():
+	var cam: Camera2D = $Player/Harpoon/Projectile/Camera2D
+	cam.limit_right = width
+	cam.limit_bottom = height
 
 func set_letters(letters):
 	for child in get_children():
