39e321a3b691fd0d4dba2ca902755bb663f09b1a
Alexis Schad <a@mandelbro.com>
2025-04-08 00:02:40 +0200

    oxygen game over

--- gd_project/GUI/game_over.gd
+++ gd_project/GUI/game_over.gd
@@ -1,10 +1,9 @@
 extends CanvasLayer
 
-@onready var total_points: float = 0.0
 signal replay()
 
 func initialize(points: float) -> void:
-	$Score.text = "Total points: " + str(total_points)
+	$Score.text = "Total points: " + str(points)
 	$AnimatedSprite2D.play()
 
 
