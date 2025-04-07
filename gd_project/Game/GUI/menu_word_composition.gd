36a9f584f1825014f9ee5ec375d65faefb039045
tyra <someonefefe@gmail.com>
2025-04-07 03:22:35 +0200

    link to letters and base score display

--- gd_project/Game/GUI/menu_word_composition.gd
+++ gd_project/Game/GUI/menu_word_composition.gd
@@ -1,25 +1,27 @@
 extends Control
 
 ## Dict de la forme : Lettre: [points, texture]
-const LETTER_POOL: Array[Array] = [
-		["L", 1, 0],
-		["O", 5, 1],
-		["V", 1, 2],
-		["E", 1, 3],
-		["U", 1, 0],
-		["P", 5, 1],
-		["A", 1, 2],
-		["B", 1, 3],
-		["C", 1, 0],
-		["D", 5, 1],
-		["F", 1, 2],
-		["G", 1, 3],
-		["H", 1, 0],
-		["O", 5, 1],
-		["V", 1, 2],
-		["E", 1, 3]
-]
+#const LETTER_POOL: Array[Array] = [
+		#["L", 1, 0],
+		#["O", 5, 1],
+		#["V", 1, 2],
+		#["E", 1, 3],
+		#["U", 1, 0],
+		#["P", 5, 1],
+		#["A", 1, 2],
+		#["B", 1, 3],
+		#["C", 1, 0],
+		#["D", 5, 1],
+		#["F", 1, 2],
+		#["G", 1, 3],
+		#["H", 1, 0],
+		#["O", 5, 1],
+		#["V", 1, 2],
+		#["E", 1, 3]
+#]
 
+
+	
 @onready var UI_LETTER = preload("res://Game/GUI/ui_letter.tscn")
 
 @onready var pool_container = $CenterContainer/VBoxContainer/PoolContainer
@@ -32,6 +34,8 @@ const LETTER_POOL: Array[Array] = [
 signal on_word_confirmed
 signal on_menu_closed
 
+static var dico: DictionaryHelper = DictionaryHelper.new(DictionaryHelper.Language.English)
+
 func _ready():
 	initialize()
 	
@@ -42,34 +46,50 @@ func _process(delta: float):
 		close_menu()
 
 func initialize():
-	setup_letter_pool(LETTER_POOL)
+	var word: Array[Letter] = [];
+	word.append(Letter.new(Alphabet.get_character("H")))
+	word.append(Letter.new(Alphabet.get_character("E")))
+	word.append(Letter.new(Alphabet.get_character("L"), Letter.FishType.Medusa, Letter.BonusType.LetterMult1))
+	word.append(Letter.new(Alphabet.get_character("L"), Letter.FishType.Eel, Letter.BonusType.WordMult1))
+	word.append(Letter.new(Alphabet.get_character("O")))
+	setup_letter_pool(word)
 
-func setup_letter_pool(letters : Array[Array]):
+func setup_letter_pool(letters : Array[Letter]):
 	for l in letters:
-		var letter = UI_LETTER.instantiate()
-		letter.initialize(l[0], l[1], l[2])
-		letter.on_letter_selected.connect(on_letter_selected.bind(letter))
-		grid_container.add_child(letter)
+		var letter_ui = UI_LETTER.instantiate()
+		letter_ui.initialize(l)
+		letter_ui.on_letter_selected.connect(on_letter_selected.bind(letter_ui))
+		grid_container.add_child(letter_ui)
 
 func on_letter_selected(letter : Control):
 	if grid_container.get_children().has(letter):
 		grid_container.remove_child(letter)
 		word_container.add_child(letter)
-		#calc_score(letter[1])
 	elif word_container.get_children().has(letter):
 		word_container.remove_child(letter)
 		grid_container.add_child(letter)
+	var word = get_word()
+	if(len(word) and dico.is_word_valid(word)):
+		$CenterContainer/VBoxContainer/Submit.disabled = false
+	else:
+		$CenterContainer/VBoxContainer/Submit.disabled = true
+	update_score()
 
 func close_menu():
 	on_menu_closed.emit()
 
-func confirm_word():
+func get_word():
 	if word_container.get_child_count() <= 0:
-		return
+		return []
 	
-	var word = ""
+	var word: Array[Letter]
 	for l in word_container.get_children():
-		word += l.get_letter()	
+		word.append(l.get_letter())
+		
+	return word
+		
+func confirm_word():
+	var word = get_word()
 		
 	for child in word_container.get_children():
 		child.queue_free()
@@ -79,9 +99,12 @@ func confirm_word():
 	if pool_container.get_child_count() <= 0:
 		on_menu_closed.emit()
 
-func calc_score(letter_points: int) -> int:
-	score += letter_points
-	return 1
+func update_score():
+	var word = get_word()
+	var score = 0
+	for letter in word:
+		score += letter.character.base_value
+	$CenterContainer/VBoxContainer/Score/Points.text = str(score)
 	
 	
 	
--- gd_project/Game/GUI/ui_letter.gd
+++ gd_project/Game/GUI/ui_letter.gd
@@ -2,14 +2,28 @@ extends Control
 
 signal on_letter_selected
 
-func initialize(letter : String, points: int, texture: int):
-	%Letter.text = letter
-	%Points.text = str(points)
-	$Button.texture_normal.set_region(Rect2(texture * 24, 0, 24, 27))
+var letter: Letter;
+
+func initialize(letter_ : Letter):
+	letter = letter_
+	%Letter.text = letter.character.character
+	%Points.text = str(letter.character.base_value)
+	var texture_index = 0
+	match letter.fish_type:
+		Letter.FishType.Medusa:
+			texture_index = 1
+		Letter.FishType.Crab:
+			texture_index = 2
+		Letter.FishType.Eel:
+			texture_index = 3
+		Letter.FishType.Clown:
+			texture_index = 0
+			
+	$Button.texture_normal.set_region(Rect2(texture_index * 24, 0, 24, 27))
 
 
-func get_letter() -> String:
-	return %Letter.text
+func get_letter() -> Letter:
+	return letter
 	
 func _on_button_pressed():
 	on_letter_selected.emit()
