bb7bd42115bbea59f61b427e3f411f7bac7d7a86
tyra <someonefefe@gmail.com>
2025-04-07 02:01:15 +0200

    Fabrication des mots

--- gd_project/Game/GUI/menu_word_composition.gd
+++ gd_project/Game/GUI/menu_word_composition.gd
@@ -1,9 +1,33 @@
 extends Control
 
+## Dict de la forme : Lettre: [points, texture]
+const LETTER_POOL: Array[Array] = [
+		["L", 1, 0],
+		["O", 5, 1],
+		["V", 1, 2],
+		["E", 1, 3],
+		["U", 1, 0],
+		["P", 5, 1],
+		["A", 1, 2],
+		["B", 1, 3],
+		["C", 1, 0],
+		["D", 5, 1],
+		["F", 1, 2],
+		["G", 1, 3],
+		["H", 1, 0],
+		["O", 5, 1],
+		["V", 1, 2],
+		["E", 1, 3]
+]
+
 @onready var UI_LETTER = preload("res://Game/GUI/ui_letter.tscn")
 
 @onready var pool_container = $CenterContainer/VBoxContainer/PoolContainer
 @onready var word_container = $CenterContainer/VBoxContainer/WordContainer
+@onready var grid_container = $CenterContainer/VBoxContainer/GridContainer
+
+@onready var multi: int = 10
+@onready var score: int = 0
 
 signal on_word_confirmed
 signal on_menu_closed
@@ -18,23 +42,23 @@ func _process(delta: float):
 		close_menu()
 
 func initialize():
-	setup_letter_pool("AJAK")
-	#setup_letter_pool(level.get_letter_pool())
+	setup_letter_pool(LETTER_POOL)
 
-func setup_letter_pool(letters : String):
+func setup_letter_pool(letters : Array[Array]):
 	for l in letters:
 		var letter = UI_LETTER.instantiate()
-		letter.initialize(l)
+		letter.initialize(l[0], l[1], l[2])
 		letter.on_letter_selected.connect(on_letter_selected.bind(letter))
-		pool_container.add_child(letter)
+		grid_container.add_child(letter)
 
 func on_letter_selected(letter : Control):
-	if pool_container.get_children().has(letter):
-		pool_container.remove_child(letter)
+	if grid_container.get_children().has(letter):
+		grid_container.remove_child(letter)
 		word_container.add_child(letter)
+		#calc_score(letter[1])
 	elif word_container.get_children().has(letter):
 		word_container.remove_child(letter)
-		pool_container.add_child(letter)
+		grid_container.add_child(letter)
 
 func close_menu():
 	on_menu_closed.emit()
@@ -54,4 +78,44 @@ func confirm_word():
 	
 	if pool_container.get_child_count() <= 0:
 		on_menu_closed.emit()
+
+func calc_score(letter_points: int) -> int:
+	score += letter_points
+	return 1
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
--- gd_project/Game/GUI/ui_letter.gd
+++ gd_project/Game/GUI/ui_letter.gd
@@ -2,8 +2,10 @@ extends Control
 
 signal on_letter_selected
 
-func initialize(letter : String):
+func initialize(letter : String, points: int, texture: int):
 	%Letter.text = letter
+	%Points.text = str(points)
+	$Button.texture_normal.set_region(Rect2(texture * 24, 0, 24, 27))
 
 
 func get_letter() -> String:
